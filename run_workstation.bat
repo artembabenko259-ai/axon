cd991e2e2ba4944cc4fb4590caaa0fc8d3a916fb
Red_Artem39 <artembabenko259@gmail.com>
2026-07-07 23:03:45 +0300

    fix: update run_workstation.bat to use self-contained build folder

--- run_workstation.bat
+++ run_workstation.bat
@@ -1,5 +1,4 @@
 @echo off
-set PATH=C:\Qt\6.11.1\mingw_64\bin;C:\Qt\Tools\mingw1310_64\bin;%PATH%
-set QT_QML_IMPORT_PATH=C:\Qt\6.11.1\mingw_64\qml
 echo Starting AXON Workstation...
-start "" "workstation\build\axon-workstation.exe"
+cd workstation\build
+start "" "axon-workstation.exe"
